fa1c423a4d4a575fb550bdc998b30d08bee0a6b2
Andri Firmansyah <ahsyandri@gmail.com>
2021-10-08 11:13:45 +0700

    menambahkan template admin LTE

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Firmansyah ID
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021-2025 <a href="https://adminlte.io">Aplikasi Presensi sederhana</a>.</strong> All rights reserved.
+  </footer>
+</div>
